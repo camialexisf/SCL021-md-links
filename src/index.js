98915ff1dfc22cc3c0cd3bc9c52a72e968597cc7
camialexisf <camialexisflores@gmail.com>
2022-10-11 18:20:28 -0300

    error: la funcion que recorre el archivo me devuelve un array vacio :( no se estan pusheando los links

--- src/index.js
+++ src/index.js
@@ -1,7 +1,88 @@
-/* module.exports = () => {
-  //leer la ruta
-  //Ver si es absoluta o relativo
-  //si es relativa convertir en absoluta
-}; */
+const fs = require("fs");
+const path = require("path");
+const promise = require("promise");
 
-"hola soy el index.js";
+route = process.argv[2];
+
+const routeCheck = () => {
+  fs.existsSync(route);
+};
+
+const absoluteLink = (route) => {
+  //si la ruta es absoluta, retornar
+  if (path.isAbsolute(route) === false) {
+    path.resolve(route);
+    return path.resolve(route);
+  } else {
+    return route;
+  }
+};
+
+const isFile = (path) => {
+  if (fs.statSync(path).isFile() === true) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const readDirectory = (route) => {
+  //aca quiero iterar el directorio
+  return new promise((resolve, reject) => {
+    const mdFiles = [];
+    fs.readdirSync(route, "utf-8", (err, files) => {
+      if (err) {
+        reject(err);
+      }
+      console.log("encontré estos archivos: ");
+      files.forEach((file) => {
+        if (extCheck(route) === true) {
+          console.log(file);
+          mdFiles.push(route);
+        }
+      });
+      console.log("analisis");
+      resolve(mdFiles);
+    });
+  });
+};
+
+const extCheck = (route) => {
+  //Aca quiero chequear la extension del archivo
+  //si la extension es .md retorna true
+  if (path.extname(route) === ".md") {
+    return true;
+  } else {
+    //si la extension no es .md retorna false
+    return false;
+  }
+};
+const findUrl = (mdFiles) => {
+  return new promise((resolve, reject) => {
+    const urlArray = [];
+    const urlRegex =
+      /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
+    fs.readFile(mdFiles, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else if (data.match(urlRegex) === null) {
+        reject("No se encuentran links en el documento");
+        return;
+      } else if (data) {
+        data.match(urlRegex).forEach((link) => {
+          urlArray.push(link);
+        });
+        return urlArray;
+      }
+    });
+  });
+};
+
+module.exports = {
+  routeCheck,
+  absoluteLink,
+  isFile,
+  readDirectory,
+  extCheck,
+  findUrl,
+};
--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,16 +1,44 @@
 #!/usr/bin/env node
-const { routeCheck, absoluteLink } = require("./components");
+const {
+  routeCheck,
+  absoluteLink,
+  isFile,
+  readDirectory,
+  extCheck,
+  findUrl,
+} = require("./index.js");
 
 const route = process.argv[2];
 const absoluteUserInput = absoluteLink(route);
 
 const mdLinks = () => {
   return new Promise((resolve, reject) => {
-    if (routeCheck(route) === false) {
-      reject("la ruta no es valida :(");
-    } else {
-      absoluteUserInput;
+    const urlArray = [];
+    if (absoluteUserInput) {
+      //aca se comprobará si es directory es true
+      if (isFile(absoluteUserInput) === true) {
+        //chequear si es extension .md
+        if (extCheck(absoluteUserInput) === false) {
+          reject(new Error("no es un archivo .md"));
+          return;
+        } else {
+          //es archivo .md por lo tanto se recorre el archivo para buscar los links
+          findUrl(absoluteUserInput).then((url) => {
+            url.forEach((link) => {
+              urlArray.push(link);
+            });
+          });
+          resolve(console.log(urlArray));
+        }
+      } else {
+        return console.log("hola " + readDirectory(absoluteUserInput));
+      }
     }
   });
 };
-mdLinks();
+
+mdLinks()
+  .then(() => {
+    console.log("funciona?");
+  })
+  .catch((err) => console.log(err.message));
